8872041ff73e7a665e7513fe1eea9c56d3cf5cf4
deer000 <camel000k@gmail.com>
2019-11-10 05:00:07 +0900

    Portfolio_profile

---
+++ main.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector(".btn");
+const openbtn = document.querySelector(".btn>button");
+
+const opennav = () => {
+  btn.classList.toggle("open");
+};
+
+openbtn.addEventListener("click", opennav);
+
+const plPage = document.querySelector(".grid_Modal_1");
+const close = document.querySelector(".close1");
+const close2 = document.querySelector(".close2");
+
+const modal1 = () => {
+  plPage.style.left = "0";
+};
+
+const closeModel1 = () => {
+  plPage.style.left = "-100%";
+};
+const closeModel2 = () => {
+  plPage2.style.left = "-100%";
+};
+close.addEventListener("click", closeModel1);
+close2.addEventListener("click", closeModel2);
+
+const plPage2 = document.querySelector(".Portfolio");
+const modal2 = () => {
+  plPage2.style.left = "0";
+};
