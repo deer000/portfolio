60588e9466854127550571c3b865d58cd01a688a
deer000 <camel000k@gmail.com>
2019-11-10 19:07:41 +0900

    portFolio_page4

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ const opennav = () => {
 openbtn.addEventListener("click", opennav);
 
 const plPage = document.querySelector(".grid_Modal_1");
+const plPage2 = document.querySelector(".grid_Modal_2");
+const plPage3 = document.querySelector(".grid_Modal_3");
+const plPage4 = document.querySelector(".grid_Modal_4");
 const close = document.querySelector(".close1");
 const close2 = document.querySelector(".close2");
 const close3 = document.querySelector(".close3");
-const plPage3 = document.querySelector(".grid_Modal_3");
-const plPage2 = document.querySelector(".grid_Modal_2");
+const close4 = document.querySelector(".close4");
 
 const modal1 = () => {
   plPage.style.left = "0";
@@ -27,10 +29,14 @@ const closeModel2 = () => {
 const closeModal3 = () => {
   plPage3.style.left = "-100%";
 };
+const closeModal4 = () => {
+  plPage4.style.left = "-100%";
+};
 
 close.addEventListener("click", closeModel1);
 close2.addEventListener("click", closeModel2);
 close3.addEventListener("click", closeModal3);
+close4.addEventListener("click", closeModal4);
 
 const modal2 = () => {
   plPage2.style.left = "0";
@@ -38,4 +44,8 @@ const modal2 = () => {
 
 const modal3 = () => {
   plPage3.style.left = "0";
+};
+
+const modal4 = () => {
+  plPage4.style.left = "0";
 };
