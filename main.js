2047a814294471ac624b34902ef78c5e2cc6e867
deer000 <camel000k@gmail.com>
2019-11-10 18:08:21 +0900

    portfolio_page3

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ openbtn.addEventListener("click", opennav);
 const plPage = document.querySelector(".grid_Modal_1");
 const close = document.querySelector(".close1");
 const close2 = document.querySelector(".close2");
+const close3 = document.querySelector(".close3");
+const plPage3 = document.querySelector(".grid_Modal_3");
+const plPage2 = document.querySelector(".grid_Modal_2");
 
 const modal1 = () => {
   plPage.style.left = "0";
@@ -21,10 +24,18 @@ const closeModel1 = () => {
 const closeModel2 = () => {
   plPage2.style.left = "-100%";
 };
+const closeModal3 = () => {
+  plPage3.style.left = "-100%";
+};
+
 close.addEventListener("click", closeModel1);
 close2.addEventListener("click", closeModel2);
+close3.addEventListener("click", closeModal3);
 
-const plPage2 = document.querySelector(".grid_Modal_2");
 const modal2 = () => {
   plPage2.style.left = "0";
+};
+
+const modal3 = () => {
+  plPage3.style.left = "0";
 };
